0d49390c2df932372466e2a9021a4e0e9f6a0bf2
Cavan <cavanbiggs@outlook.com>
2024-07-21 15:31:56 -0300

    Add project files.

---
+++ UnitTesting/Rocket/RocketEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket.UnitTesting
+{
+    public class RocketEngine
+    {
+        public RocketEngine() { }
+
+        public bool StartEngine(Enum command)
+        {
+            return false;
+        }
+    }
+}

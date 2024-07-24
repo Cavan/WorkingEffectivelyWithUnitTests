67bf5355c8b344311b3e8f07ea92d85d3741afb7
Cavan <cavanbiggs@outlook.com>
2024-07-23 21:32:09 -0300

    Added Domain classes

---
+++ UnitTesting/DomainCode/Rental.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.DomainCode
+{
+    public class Rental
+    {
+        internal double GetCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string GetLineItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int GetPoints()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
